d844893d235d89642ce9bd00f9d2ba21af493c60
PalomaV-M <112986916+PalomaV-M@users.noreply.github.com>
2022-11-16 21:53:15 -0700

    created hero class

---
+++ hero.py
@@ -0,0 +1,39 @@
+import random
+
+# heroes = ["Wonder Woman", "Dumbledore", "Spiderman", "Batman", "Captain Planet", "Ant Man"]
+
+class Hero:
+    def __init__(self, name, starting_health=100):
+        '''Instance properties:
+        name: String
+        starting_health: Integer
+        current_health: Integer
+        '''
+        self.name = name
+        self.starting_health = starting_health
+            # always the same as their starting health (no damage taken yet!)
+        self.current_health = starting_health
+        #                      ^^^^^^^^^^
+    
+    def fight(self, opponent):
+        '''Current hero will take turns fighting the opponent hero passed in'''
+          # TODO: Fight each hero until a victor emerges.
+            # Phases to implement:
+            #1) randomly choose winner,
+            # Hint: Look into random library, more specifically the choice method
+        fighters = [self.name, opponent.name]
+        victor = random.choice(fighters)
+        print(f"{victor} wins!")
+
+
+
+if __name__ == "__main__":
+
+    my_hero = Hero("Grace Hopper", 200)
+    print(my_hero.name)
+    print(my_hero.current_health)
+
+    hero1 = Hero("Wonder Woman")
+    hero2 = Hero("Dumbledore")
+
+    hero1.fight(hero2)
--- my_dogs.py
+++ my_dogs.py
@@ -1,9 +1,12 @@
 # my_dogs.py
-import dog
+from dog import Dog
 
-my_dog = dog.Dog("Rex", "SuperDog")
+# my_dog = dog.Dog("Rex", "SuperDog")
+my_dog = Dog("Rex", "SuperDog")
+
 my_dog.bark()
 
-my_other_dog = dog.Dog("Annie", "SuperDog")
+# my_other_dog = dog.Dog("Annie", "SuperDog")
+my_other_dog = Dog("Annie", "SuperDog")
 print(my_other_dog.name)
 
